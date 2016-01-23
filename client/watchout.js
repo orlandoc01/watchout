9c5ca1522805f586834a722be720346a1005bc56
orlandoc01 <orlandoc@sas.upenn.edu>
2016-01-23 11:08:55 -0800

    CheckFunc finds closes asteroid, will be used for collisions

--- client/watchout.js
+++ client/watchout.js
@@ -13,8 +13,10 @@ var axes = {
 
 var Player = function() {
   var playObj = {};
-  playObj.x = axes.x(0);
-  playObj.y = axes.y(0);
+  playObj.height = 150;
+  playObj.width  = 100;
+  playObj.x = axes.x(50);
+  playObj.y = axes.y(50);
   playObj.angle = 0;
   playObj.velocity = 0;
   return playObj;
@@ -24,32 +26,39 @@ var Player = function() {
 var drag = d3.behavior.drag()
   //chain stuff
   .on('drag',function(d, i){
-    d.x += d3.event.dx;
-    d.y += d3.event.dy;
+    if ( d.x + d3.event.dx < gameOptions.width - gameOptions.padding && d.x + d3.event.dx > gameOptions.padding){
+      d.x += d3.event.dx;
+    }
+    if (d.y + d3.event.dy < gameOptions.width - gameOptions.padding && d.y + d3.event.dy > gameOptions.padding){
+      d.y += d3.event.dy;
+    } 
     d3.select(this)
-    .attr('x', d.x)
-    .attr('y', d.y);
+    .attr('x', d.x - d.width/2)
+    .attr('y', d.y - d.height/2);
     // attr("transform", function(d,i) {
     //   return "translate(" + [d.x, d.y] + ")";
     // });
   });
 
 
+
 var svgContainer = d3.select(".board")
   .append('svg')
   .attr({'width': gameOptions.height, 'height': gameOptions.width});
 
+var asteroidList = [];
 
 var makeAsteroids = function(n){
 
-  return _.range(n).map( function(v) {
+  asteroidList =  _.range(n).map( function(v) {
     var obj =  { 
         id: v,
-        x: Math.ceil(Math.random()*100),
-        y: Math.ceil(Math.random()*100)
+        x: axes.x(Math.ceil(Math.random()*100)),
+        y: axes.y(Math.ceil(Math.random()*100))
       };
     return obj;
   });
+  return asteroidList;
 };
 
 
@@ -61,8 +70,8 @@ var Asteroids = function(){
   //update Asteroid Positions
   asteroidMapping.transition()
     .duration(1000)
-    .attr('x', (ast) => axes.x(ast.x))
-    .attr('y', (ast) => axes.y(ast.y));
+    .attr('x', (ast) => ast.x)
+    .attr('y', (ast) => ast.y);
       // .tween('custom', function(t){
       //   var asteroid = d3.select(this);
       //   var startX = asteroid.attr('x');
@@ -74,13 +83,13 @@ var Asteroids = function(){
   asteroidMapping.enter()
     .append('image')
     .attr('class', 'asteroid')
-    .attr('x', (ast) => axes.x(ast.x))
-    .attr('y', (ast) => axes.y(ast.y))
+    .attr('x', (ast) => ast.x)
+    .attr('y', (ast) => ast.y)
     .attr("xlink:href", "asteroid.png")
     .attr("class", "asteroid");
   
 
-    setTimeout(Asteroids, 1000);
+    //setTimeout(Asteroids, 1000);
 };
 
 Asteroids();
@@ -91,22 +100,38 @@ var rocket = svgContainer.selectAll('image.rocket')
   .append("image")
   .attr('class', 'rocket')
   .attr("xlink:href", "rocket.gif")
-  .attr("height", "150px")
-  .attr("width", "100px")
+  .attr("height", function(d) {
+    return d.height ;
+  })
+  .attr("width", function(d) {
+    return d.width ;
+  })
   .attr("x", function(d) {
-    return d.x ;
+    return d.x - d.width/2 ;
   })
   .attr("y", function(d) {
-    return d.y;
+    return d.y - d.height/2;
   })
-  .attr("transform", "translate(" + axes.x(50) + "," + axes.y(50) + ")")
   .call(drag);
 
 var checkFunc = function(){
-  var asteroids = svgContainer.selectAll('image.asteroid')
-    .attr('x');
-  console.log(asteroids);
+  var squareDistance = 0;
+  var closestAsteroid = asteroidList.reduce( function(prevA, A) {
+    DistPrev = Math.pow((rocketInstance.x - prevA.x),2) + Math.pow((rocketInstance.y - prevA.y),2);
+    DistA = Math.pow((rocketInstance.x - A.x),2) + Math.pow((rocketInstance.y - A.y),2);
+    if(DistA < DistPrev) {
+      squareDistance = DistA;
+      return A; 
+    } else {
+      squareDistance = DistPrev;
+      return prevA;
+    } 
+  });
+
+  console.log("Closest Asteroid is has x = " + closestAsteroid.x + " and y = " + closestAsteroid.y);
+  console.log("Square is " + squareDistance);
+  
 };
 
-//setInterval(checkFunc, 5);
+setInterval(checkFunc, 5000);
 
