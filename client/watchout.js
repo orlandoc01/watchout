8ac393045cc7bb98111ec2a99064a022053a6fa4
orlandoc01 <orlandoc@sas.upenn.edu>
2016-01-22 16:53:49 -0800

    Appends all new asteroids to SVGcontainer when run

--- client/watchout.js
+++ client/watchout.js
@@ -1,3 +1,38 @@
+function GameClass() {
+  this.attributes = {};
+  this.attributes.x;
+  this.attributes.y; 
+  this.attributes.height; 
+  this.attributes.width;
+  this.angle = 0;
+  this.velocity = 0;
+};
+
+
+function Asteroid() {
+
+  GameClass.call(this);
+  this.attributes.x = axes.x(Math.floor(Math.random()*101));
+  this.attributes.y = axes.y(Math.floor(Math.random()*101));
+  this.attributes.height = Math.floor(Math.random()*40 + 10) + 'px';
+  this.attributes.width = this.attributes.height;
+  this.attributes.href = 'asteroid.png';
+  //this.move();
+};
+
+Asteroid.prototype = Object.create(GameClass.prototype);
+Asteroid.prototype.move = function(){
+  //this.cx = //??
+  //this.cy = //??
+  setTimeout(this.move,1000);
+};
+
+
+
+
+
+
+
 // start slingin' some d3 here.
 var gameOptions = {
   height: 500,
@@ -18,40 +53,26 @@ var svgContainer = d3.select(".board")
   .attr({'width': gameOptions.height, 'height': gameOptions.width});
 
 
-var asteroidData = [];
+var asteroidList = _.range(gameOptions.nAsteroids).map( (_) => {return new Asteroid()});
+
+var wtf = svgContainer.selectAll('image').data(asteroidList)
+            .enter()
+            .append( function(d) {
+              var image = document.createElement("IMAGE");
+              _.each(d.attributes, function(value, key) {
+                image.setAttribute(key, value);
+              });
+              return image;
+            });
 
 
-function GameClass(cx, cy) {
-  this.cx = cx;
-  this.cy = cy; 
-  this.angle = 0;
-  this.velocity = 0;
-}
 
 
-
-function Asteroid() {
-  GameClass.call(this);
-}
-Asteroid.prototype = Object.create(GameClass.prototype);
-
-function Player() {
-  GameClass.call(this);
-
-}
-Player.prototype = Object.create(GameClass.prototype);
-
-
-var astroid = svgContainer.append('image')
-  .attr({"xlink:href": "asteroid.png",
-    "height": "100px",
-    "width": "100px"
-    });
 
 var rocket = svgContainer.append('image')
   .attr({"xlink:href": "rocket.gif",
     "height": "100px",
     "width": "100px",
-    "x": axes.x(50) - this.width,
-    "y": axes.y(50) - this.height,
+    "x": axes.x(50),
+    "y": axes.y(50),
     });
